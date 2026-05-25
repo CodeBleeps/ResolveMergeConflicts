16dd3a80a50f8b7248ab814bb0df158dfd7a26ba
Ishwar <ishwar_chaudhari@mahadiscom.in>
2026-05-25 14:47:20 +0530

    Branch 2 changes

--- src/main/java/com/msedcl/main/ResolveMergeConflictMain.java
+++ src/main/java/com/msedcl/main/ResolveMergeConflictMain.java
@@ -5,7 +5,7 @@ public class ResolveMergeConflictMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ResolveMergeConflict Main");
-		System.out.println("ResolveMergeConflict Branch 1 Main");
+		System.out.println("ResolveMergeConflict Branch 2 Main");		
 
 	}
 
