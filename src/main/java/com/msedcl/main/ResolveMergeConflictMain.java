11503f42d42b46b4241218518fbbdc12a08aef84
IndResh <ibpardeshi@mahadiscom.in>
2026-05-25 14:41:14 +0530

    Fresh

---
+++ src/main/java/com/msedcl/main/ResolveMergeConflictMain.java
@@ -0,0 +1,11 @@
+package com.msedcl.main;
+
+public class ResolveMergeConflictMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ResolveMergeConflict Main");
+
+	}
+
+}
